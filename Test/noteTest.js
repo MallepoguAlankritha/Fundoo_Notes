1ca56869ae2dbde9f87bd5422bf5d32998140dcf
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-12-29 20:04:53 +0530

    Feat: Check validation of given false id

--- Test/noteTest.js
+++ Test/noteTest.js
@@ -575,7 +575,7 @@ describe("DeleteNoteById", () => {
         return done();
       });
   });
-  it.only("when it is validate with the given id, should return proper response from controller", (done) => {
+  it("when it is validate with the given id, should return proper response from controller", (done) => {
     const token = noteData.notes.validToken;
     chai
       .request(server)
@@ -587,6 +587,21 @@ describe("DeleteNoteById", () => {
           return done();
         }
         res.should.have.status(201);
+        return done();
+      });
+  });
+  it.only("when it is validate with the given false id, should return proper response from controller", (done) => {
+    const token = noteData.notes.validToken;
+    chai
+      .request(server)
+      .delete("/deleteNote/61c8913bdcf696aca")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
         return done();
       });
   });
